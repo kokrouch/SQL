1a0577a36cad03bb5ef9f0e402eba1158dc62cc0
kokrouch <46744584+kokrouch@users.noreply.github.com>
2021-11-26 15:18:20 +0300

    Delete Fourth_query.sql

--- Fourth_query.sql
+++
@@ -1,9 +0,0 @@
-SELECT classroom 
-FROM Schedule
-GROUP BY classroom
-HAVING COUNT(classroom) = 
-    (SELECT COUNT(classroom) 
-     FROM Schedule 
-     GROUP BY classroom
-     ORDER BY COUNT(classroom) DESC 
-     LIMIT 1)
