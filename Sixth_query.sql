30088fe1da7ccaa9b11d90d141a4764a713ea636
kokrouch <46744584+kokrouch@users.noreply.github.com>
2021-11-26 15:18:35 +0300

    Delete Sixth_query.sql

--- Sixth_query.sql
+++
@@ -1,5 +0,0 @@
-with cte as (select row_number() over(order by [date], name) rn, count(*) over() cnt, [date], [name]
-from [dbo].[Battles])
-select a.[rn], a.[name],a.[date],  b.[rn], b.[name], b.[date]
-from cte a left join cte b on a.[rn] = b.rn - ([b].cnt / 2 + b.cnt % 2)
-where a.[rn] <= a.cnt / 2 + a.cnt % 2
